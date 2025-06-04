15bc2b709ea9fd81b2fd57e702a1febd7ead410e
RuiLazy <vietducluxury68@gmail.com>
2025-06-05 03:26:10 +0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,8 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+robuxsieure-frontend/
+├── index.html
+├── package.json
+├── vite.config.js
+└── src/
+    ├── main.jsx ✅
+    ├── App.jsx
+    └── index.css
